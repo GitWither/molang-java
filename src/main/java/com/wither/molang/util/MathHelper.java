9ecb4cec7517758584456a29590d81b8ea465bd0
Wither <wither.tweets@gmail.com>
2020-08-29 09:52:18 +0200

    Changed function names to match java conventions in MathHelper

--- src/main/java/com/wither/molang/util/MathHelper.java
+++ src/main/java/com/wither/molang/util/MathHelper.java
@@ -1,15 +1,21 @@
 package com.wither.molang.util;
 
 public class MathHelper {
-    public static float Clamp(float value, float min, float max) {
+    public static float clamp(float value, float min, float max) {
         return (value < min) ? min : (max < value) ? max : value;
     }
 
-    public static float Lerp(float a, float b, float t) {
+    public static float lerp(float a, float b, float t) {
         return (1 - t) * a + t * b;
     }
 
-    public static float HermiteBlend(float value) {
+    public static float hermiteBlend(float value) {
         return (float) (Math.pow(3F * value, 2F) - Math.pow(2F * value, 3F));
     }
+
+    //TODO: Implement die roll
+    //TODO: Implement integer die roll
+    //TODO: Implement lerprotate
+    //TODO: Implement random integer range
+    //TODO: Implement random float range
 }
