e2f8b67923a6d55beb302da3805512a8d5f8e29c
Piotr Brzozowski <brzozowski.s.piotr@gmail.com>
2020-09-07 21:35:32 +0200

    Remove old commented code

--- src/main/java/com/wither/molang/Main.java
+++ src/main/java/com/wither/molang/Main.java
@@ -1,32 +1,7 @@
 package com.wither.molang;
 
-import com.wither.molang.objects.MoLangObject;
-import com.wither.molang.objects.MoLangPrimitive;
-
 public class Main {
     public static void main(String[] args) {
-//        String string = "2 + (variable.test.a * 4)";
-        /*
-        String string = "math.acos(0.5)";
-        MoLangLexer moLangLexer = new MoLangLexer(CharStreams.fromString(string));
-        CommonTokenStream tokens = new CommonTokenStream(moLangLexer);
-        MoLangParser parser = new MoLangParser(tokens);
-        Stack<Float> stack = new Stack<>();
-        MoLangObject scope = new MoLangObject(
-                "query", new MoLangObject(),
-                "context", new MoLangObject(),
-                "temp", new MoLangObject(),
-                "math", new MoLangMath(),
-                "variable", new MoLangObject(
-                        "test", new MoLangObject(
-                                "a", new MoLangPrimitive(3f)
-                        )
-                )
-        );
-        parser.addParseListener(new MoLangBasicListener(stack, scope));
-        parser.program();
-        System.out.println(stack);
-         */
         MoLang moLang = new MoLang("math.pi");
         System.out.println(moLang.evaluate());
     }
